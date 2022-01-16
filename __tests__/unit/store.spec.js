894f93b412f8ac0d7a24b6dfe50e4ad727180871
IkireziDivine01 <ikirezidivine21@gmail.com>
2022-01-16 20:34:08 +0200

    feat: add, delete, like test

--- __tests__/unit/store.spec.js
+++ __tests__/unit/store.spec.js
@@ -1,6 +1,7 @@
 import testimonialsStore from "../../src/store/testimonialStore";
-import {getAllTestimonials} from '../../src/actions/testimonialActions';
+import {addTestimonial, deleteTestimonial, likeTestimonial} from '../../src/actions/testimonialActions';
 
+// add,delete,like
 
 describe('testmonial store', () => {
 
@@ -33,5 +34,46 @@ describe('testmonial store', () => {
         ]
         
         expect(testimonialsStore.getState()).toEqual(initialState)
+    })
+
+    it("add testimonials test", async () => {
+        const newTestimony =    {
+                id: 3,
+                OwnerFullName: "Divine elsa",
+                OwnerId: 1,
+                testimonialType: "illiness",
+                testimonialTitle: "23 Years On Bed",
+                testimonialBody: "Jamaica Hospital Medical Center regularly receives letters of thanks from former patients or their family members for the high level of care we provide throughout our facility.",
+                views: 1,
+                likes: 0,
+                dislikes: 0
+            }
+        
+
+        const initialLength = testimonialsStore.getState().length  // number of testimonials before addition
+        
+        await testimonialsStore.dispatch(addTestimonial(newTestimony))
+
+        expect(testimonialsStore.getState().length).toBeGreaterThan(initialLength)
+        
+        expect(testimonialsStore.getState()[2].OwnerFullName).toEqual(newTestimony.OwnerFullName)
+    })
+
+    it("delete testimonials", async () => {
+
+        const initialLength = testimonialsStore.getState().length
+
+        await testimonialsStore.dispatch(deleteTestimonial(testimonialsStore.getState()[1]))
+
+        expect(testimonialsStore.getState().length).toBeLessThan(initialLength)
+    })
+
+    it("like testimonials", async () =>{
+        const testimonialToLike = testimonialsStore.getState()[0]
+
+        await testimonialsStore.dispatch(likeTestimonial(testimonialToLike))
+
+        expect(testimonialsStore.getState()[0].likes).toBeGreaterThan(testimonialToLike.likes)
+        
     })
 })
