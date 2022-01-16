b34ecfe9dbfc4815e502298ee84b2035f8def6a4
claranceliberi <liberintwari@gmail.com>
2022-01-16 19:10:20 +0200

    test: initial test

--- __tests__/unit/store.spec.js
+++ __tests__/unit/store.spec.js
@@ -5,26 +5,33 @@ import {getAllTestimonials} from '../../src/actions/testimonialActions';
 describe('testmonial store', () => {
 
 
-    it('dispatches termonislas', () => {
+    it('Initial State test', () => {
         // Response body sample
-        // const response = [
-        //     {
-        //         title: 'The Master and Margarita',
-        //         author: 'Mikhail Bulgakov',
-        //         isbn: '0679760806'
-        //     },
-        //     {
-        //         title: '1984',
-        //         author: 'George Orwell',
-        //         isbn: '0451524934'
-        //     }
-        // ]
-
-        // const expectedActions = [
-        //  { type: booksActions.BOOKS_REQUEST, payload: undefined},
-        //  { type: booksActions.BOOKS_SUCCESS, payload: { results: response}}
-        // ]
-        console.log(testimonialsStore.dispatch(getAllTestimonials))
-
+        const initialState = [
+            {
+                id: 1,
+                OwnerFullName: "ntwari egide",
+                OwnerId: 1,
+                testimonialType: "illiness",
+                testimonialTitle: "23 Years On Bed",
+                testimonialBody: "Jamaica Hospital Medical Center regularly receives letters of thanks from former patients or their family members for the high level of care we provide throughout our facility.",
+                views: 1,
+                likes: 0,
+                dislikes: 0
+            },
+            {
+                id: 2,
+                OwnerFullName: "mugisha jules",
+                OwnerId: 1,
+                testimonialType: "illiness",
+                testimonialTitle: "23 Years On Bed",
+                testimonialBody: "Jamaica Hospital Medical Center regularly receives letters of thanks from former patients or their family members for the high level of care we provide throughout our facility.",
+                views: 1,
+                likes: 0,
+                dislikes: 0
+            }
+        ]
+        
+        expect(testimonialsStore.getState()).toEqual(initialState)
     })
 })
